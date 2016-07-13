53d9c9c3e6e2374199926acfe48e1747bf7f608d
Irasov Igor <srnan@yandex.ru>
2016-07-13 17:16:32 +0600

    typo

--- FactoryMethod/src/com/irasov/factorymethod/creator/FactoryMethod.java
+++ FactoryMethod/src/com/irasov/factorymethod/creator/FactoryMethod.java
@@ -2,9 +2,9 @@ package com.irasov.factorymethod.creator;
 
 public class FactoryMethod {
 	public static final String ROME_WATCH = "rome";
-	public static final String DIGIATL_WATTCH = "digital";
+	public static final String DIGIATL_WATCH = "digital";
 	public static Factory CreateWatch(String type){
-		if (type.equals(DIGIATL_WATTCH)){
+		if (type.equals(DIGIATL_WATCH)){
 			return new CreateDigitalWatch();
 		}
 		else if(type.equals(ROME_WATCH)){
